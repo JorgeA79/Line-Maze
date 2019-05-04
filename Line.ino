f3481029b644666c441abb77f8a5faa8edfb1ce4
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-05-03 22:34:09 -0500

    Update Line.ino

--- Line.ino
+++ Line.ino
@@ -1,11 +1,20 @@
-int A,B,C,D;
+int A,B,C;
 void setup() {
  pinMode(12,OUTPUT);
+ //WheelRB
  pinMode(13,OUTPUT);
+ //WheelLB
+ pinMode(14,OUTPUT);
+ //WheelRF
+ pinMode(15,OUTPUT);
+ //WheelLF
+ 
  pinMode(2,INPUT);
+ //SensorCenter
  pinMode(3,INPUT);
+ //SensorRight
  pinMode(4,INPUT);
- pinMode(5,INPUT);  // put your setup code here, to run once:
+ //SensorLeft        
 
 }
 
@@ -13,53 +22,52 @@ void loop() {
 A=digitalRead(2);
 B=digitalRead(3);
 C=digitalRead(4);
-D=digitalRead(5);// put your main code here, to run repeatedly:
 
- if(A==0 && B==0 && C==0 && D==0)
- {digitalWrite(12,1);
- digitalWrite(13,1);
- }
- if(A==1 && B==1 && C==1 && D==1)
- {digitalWrite(12,1);
- digitalWrite(13,1);
- }
-  if(A==0 && B==1 && C==1 && D==1)
+
+ 
+  //No No No
+ if(A==0 && B==0 && C==0)
  {digitalWrite(12,0);
- digitalWrite(13,1);
- }
- if(A==1 && B==0 && C==1 && D==1)
- {digitalWrite(12,0);
- digitalWrite(13,1);
- }
-  if(A==0 && B==0 && C==1 && D==1)
- {digitalWrite(12,0);
- digitalWrite(13,1);
- }
-  if(A==0 && B==0 && C==0 && D==1)
- {digitalWrite(12,0);
- digitalWrite(13,1);
- }
-  if(A==0 && B==0 && C==1 && D==0)
- {digitalWrite(12,0);
- digitalWrite(13,1);
- }
-  if(A==1 && B==1 && C==0 && D==1)
- {digitalWrite(12,1);
  digitalWrite(13,0);
+ digitalWrite(14,0);
+ digitalWrite(15,0);
  }
-  if(A==1 && B==1 && C==1 && D==0)
- {digitalWrite(12,1);
- digitalWrite(13,0);
- }
-  if(A==1 && B==1 && C==0 && D==0)
- {digitalWrite(12,1);
- digitalWrite(13,0);
- }
-  if(A==1 && B==0 && C==0 && D==0)
- {digitalWrite(12,1);
- digitalWrite(13,0);
- }
-  if(A==0 && B==1 && C==0 && D==0)
+  //Si Si Si
+ if(A==1 && B==1 && C==1)
  {digitalWrite(12,1);
  digitalWrite(13,1);
+ digitalWrite(14,1);
+ digitalWrite(15,1);
+ }
+  //Si Si No
+  if(A==1 && B==1 && C==0)
+ {digitalWrite(12,0);
+ digitalWrite(13,1);
+  digitalWrite(14,1);
+ digitalWrite(15,1);
+ }
+ //Si No Si
+ if(A==1 && B==0 && C==1)
+ {digitalWrite(12,0);
+ digitalWrite(13,1);
+ }
+ //No Si Si
+  if(A==0 && B==1 && C==1)
+ {digitalWrite(12,0);
+ digitalWrite(13,1);
+ }
+ //Si No No
+  if(A==1 && B==0 && C==0)
+ {digitalWrite(12,0);
+ digitalWrite(13,1);
+ }
+  //No Si No
+  if(A==0 && B==1 && C==0)
+ {digitalWrite(12,0);
+ digitalWrite(13,1);
+ }
+ //No No Si
+  if(A==0 && B==0 && C==1)
+ {digitalWrite(12,1);
+ digitalWrite(13,0); 
  } }
