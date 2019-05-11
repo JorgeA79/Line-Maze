5f3f53dc9d0b985f673cf2e5e67f6c0f001c4c7a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-05-11 16:42:13 -0500

    Update Line.ino

--- Line.ino
+++ Line.ino
@@ -1,73 +1,196 @@
-int A,B,C;
-void setup() {
- pinMode(12,OUTPUT);
- //WheelRB
- pinMode(13,OUTPUT);
- //WheelLB
- pinMode(14,OUTPUT);
- //WheelRF
- pinMode(15,OUTPUT);
- //WheelLF
- 
- pinMode(2,INPUT);
- //SensorCenter
- pinMode(3,INPUT);
- //SensorRight
- pinMode(4,INPUT);
- //SensorLeft        
+int I,C,R;    //  Sensores         //
+int DB = 8;   //  Derecha Atras    //
+int DF = 7;   //  Derecha Frente   //
+int IB = 6;   //  Izquierda Atras  //
+int IF = 5;   //  Izquierda Frente //
 
+
+int L = 1;    //        Line       //
+int M = 0;    //        Maze       //
+
+//  Vars UltraSonic //
+
+const int UTI = 0;  //  Trigger Izquierdo // 
+const int UTF = 0;  //  Trigger Frente    //
+const int UTD = 0;  //  Trigger Derecho   //
+const int UEI = 0;  //  Echo Izquierdo    //
+const int UEF = 0;  //  Echo Frente       //
+const int UED = 0;  //  Echo Derecho      //
+float distance1;    //  Distancia Iz      //
+float distance2;    //  Distancia Frente  //
+float distance3;    //  Distancia Der     //
+
+//  Ultrasonicos  // 
+  float sonar(int trigger, int echo)  {
+  float sonarDistance;
+  unsigned long pulseTime;
+  const float soundSpeed = .03432;                      
+  
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(4);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW); 
+
+  pulseTime = pulseIn(echo, HIGH);
+  sonarDistance = (pulseTime*soundSpeed)/2;
+
+  return sonarDistance;         
+}
+   
+void setup() {
+  
+ // Llantas //
+ pinMode(DB,OUTPUT); // Llanta Derecha Atras    //
+ pinMode(DF,OUTPUT); // Llanta Derecha Frente   //
+ pinMode(IB,OUTPUT); // Llanta Izquierda Atras  //
+ pinMode(IF,OUTPUT); // Llanta Izquierda Frente //
+  
+ //   IRs   //
+ pinMode(2,INPUT); // Sensor Izquierdo  //
+ pinMode(3,INPUT); // Sensor Medio      //
+ pinMode(4,INPUT); // Sensor Derecho    //
+
+ //  UltraSonicos  //
+ Serial.begin (9600);
+ pinMode(UTI, OUTPUT);  //  Trigger Izquierdo //
+ pinMode(UTF, OUTPUT);  //  Trigger Frente    //
+ pinMode(UTD, OUTPUT);  //  Trigger Derecho   //
+ pinMode(UEI, INPUT);   //  Echo Izquierdo    //
+ pinMode(UEF, INPUT);   //  Echo Frente       //
+ pinMode(UED, INPUT);   //  Echo Derecho      //
 }
 
 void loop() {
-A=digitalRead(2);
-B=digitalRead(3);
-C=digitalRead(4);
+//  Vars IRs  //
+L=digitalRead(3);
+M=digitalRead(4);
+R=digitalRead(2);
+
+//  UltraSonics //
+distance1 = sonar(UTI, UEI);  //  UltraSonico Izquierdo // 
+distance2 = sonar(UTF, UEF);  //  UltraSonico Frente    // 
+distance2 = sonar(UTD, UED);  //  UltraSonico Derecho   // 
 
 
+//  Line  //
+if(L==1 && M==0)
+{
+ //ULTRASONIC FRENTE//
+ if(distance2 <= 3){
+  digitalWrite(DB,LOW);
+  digitalWrite(DF,LOW);
+  digitalWrite(IB,LOW);
+  digitalWrite(IF,LOW);
+ }
  
-  //No No No
- if(A==0 && B==0 && C==0)
- {digitalWrite(12,0);
- digitalWrite(13,0);
- digitalWrite(14,0);
- digitalWrite(15,0);
+ if(I==LOW && M==LOW && R==LOW) //  No No No //
+ {
+  digitalWrite(DB,LOW);
+  digitalWrite(DF,LOW);
+  digitalWrite(IB,LOW);
+  digitalWrite(IF,LOW);
  }
-  //Si Si Si
- if(A==1 && B==1 && C==1)
- {digitalWrite(12,1);
- digitalWrite(13,1);
- digitalWrite(14,1);
- digitalWrite(15,1);
+   
+ if(I==HIGH && C==HIGH && R==HIGH)  //  Si Si Si  //
+ {
+  digitalWrite(DB,LOW);
+  digitalWrite(DF,HIGH);
+  digitalWrite(IB,LOW);
+  digitalWrite(IF,HIGH);
  }
-  //Si Si No
-  if(A==1 && B==1 && C==0)
- {digitalWrite(12,0);
- digitalWrite(13,1);
-  digitalWrite(14,1);
- digitalWrite(15,1);
+    
+  if(I==HIGH && C==HIGH && R==LOW) // Si Si No  //
+ {
+  digitalWrite(DB,HIGH);
+  digitalWrite(DF,LOW);
+  digitalWrite(IB,LOW);
+  digitalWrite(IF,HIGH);
  }
- //Si No Si
- if(A==1 && B==0 && C==1)
- {digitalWrite(12,0);
- digitalWrite(13,1);
+
+ if(I==HIGH && C==LOW && R==HIGH) //  Si No Si  //
+ {
+  digitalWrite(DB,LOW);
+  digitalWrite(DF,HIGH);
+  digitalWrite(IB,LOW);
+  digitalWrite(IF, HIGH);
  }
- //No Si Si
-  if(A==0 && B==1 && C==1)
- {digitalWrite(12,0);
- digitalWrite(13,1);
+  
+  if(I==LOW && C==HIGH && R==HIGH) // No Si Si  //
+ {
+  digitalWrite(DB,LOW);
+  digitalWrite(DF,HIGH);
+  digitalWrite(IB,HIGH);
+  digitalWrite(IF,LOW);
  }
- //Si No No
-  if(A==1 && B==0 && C==0)
- {digitalWrite(12,0);
- digitalWrite(13,1);
+   
+  if(I==LOW && C==LOW && R==HIGH)  // No No Si //
+ {
+  digitalWrite(DB,LOW);
+  digitalWrite(DF,HIGH);
+  digitalWrite(IB,HIGH);
+  digitalWrite(IF,LOW);
+ }}
+ // Maze  //
+ 
+ if(M==1 && L==0)
+ {
+  if(distance1 <= 3 && distance2 <= 3 && distance3 <= 3) //No No No
+  {
+    digitalWrite(DB,LOW);
+    digitalWrite(DF,HIGH);
+    digitalWrite(IB,LOW);
+    digitalWrite(IF,HIGH);
+    analogWrite(enB, 128);
+  }
+  if(distance1 >= 3 && distance2 >= 3 && distance3 >= 3) //Si Si Si
+  {
+    digitalWrite(DB,HIGH);
+    digitalWrite(DF,LOW);
+    digitalWrite(IB,LOW);
+    digitalWrite(IF,HIGH);
+    analogWrite(enB, 255);
+  }
+   if(distance1 >= 3 && distance2 >= 3 && distance3 <= 3) //Si Si No
+  {
+    digitalWrite(DB,HIGH);
+    digitalWrite(DF,LOW);
+    digitalWrite(IB,LOW);
+    digitalWrite(IF,HIGH);
+    analogWrite(enB, 255);
+  }
+     if(distance1 >= 3 && distance2 <= 3 && distance3 >= 3) //Si No Si
+  {
+    digitalWrite(DB,LOW);
+    digitalWrite(DF,HIGH);
+    digitalWrite(IB,LOW);
+    digitalWrite(IF,HIGH);
+    analogWrite(enB, 255);
+  }
+     if(distance1 <= 3 && distance2 >= 3 && distance3 >= 3) //No Si Si
+  {
+    digitalWrite(DB,LOW);
+    digitalWrite(DF,HIGH);
+    digitalWrite(IB,HIGH);
+    digitalWrite(IF,LOW);
+    analogWrite(enB, 255);
+  }
+     if(distance1 <= 3 && distance2 <= 3 && distance3 >= 3) //No No Si
+  {
+    digitalWrite(DB,LOW);
+    digitalWrite(DF,HIGH);
+    digitalWrite(IB,LOW);
+    digitalWrite(IF,HIGH);
+    analogWrite(enB, 255);
+  }
+  if(distance1 >= 3 && distance2 <= 3 && distance3 <= 3) //Si No No
+  {
+    digitalWrite(DB,LOW);
+    digitalWrite(DF,HIGH);
+    digitalWrite(IB,LOW);
+    digitalWrite(IF,HIGH);
+    analogWrite(enB, 128);
+  }
  }
-  //No Si No
-  if(A==0 && B==1 && C==0)
- {digitalWrite(12,0);
- digitalWrite(13,1);
- }
- //No No Si
-  if(A==0 && B==0 && C==1)
- {digitalWrite(12,1);
- digitalWrite(13,0); 
- } }
+ 
+}
